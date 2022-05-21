f5d703bee84988e9a6da8679fcac51766ae005fe
cesarco6 <alfredocesarco@gmail.com>
2022-05-21 00:21:25 -0500

    Functionality of mobile manu first part

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,15 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu()
 
 if(module.hot) {
     module.hot.accept()
 }
 
+/* Section for functionality of the page */
+
 const d = new Date()
 document.getElementById("year").innerHTML = d.getFullYear()
+
 
