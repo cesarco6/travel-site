b825499cb77a86c3ba8df37eeb2472d9d16e1e6e
cesarco6 <alfredocesarco@gmail.com>
2022-05-28 15:28:12 -0500

    modal completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+//import Modal from './modules/Modal'
 
 
 /* section for webpack */
@@ -10,12 +11,15 @@ if(module.hot) {
 }
 
 /* Section for functionality of the page */
-
+//new Modal()
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
-
 let mobileMenu = new MobileMenu()
+let modal
+
+
+
 
 const d = new Date()
 document.getElementById("year").innerHTML = d.getFullYear()
