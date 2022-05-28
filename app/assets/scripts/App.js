7aa1b51589ef847a99a9ad4a2dfd980df35560fa
cesarco6 <alfredocesarco@gmail.com>
2022-05-28 16:53:24 -0500

    variable borrada

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -16,7 +16,6 @@ let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 let mobileMenu = new MobileMenu()
-let modal
 
 
 
